e6d591d6823d51d91055b6f5b796d808fe2bbe2c
levinatanael <levi.natanael@gmail.com>
2024-10-26 13:35:36 -0300

    Configurando Swagger e habilitando annotations para documentar API

--- Escola.Solution/Escola.WebAPI/Program.cs
+++ Escola.Solution/Escola.WebAPI/Program.cs
@@ -4,6 +4,7 @@ using Escola.Domain.Interfaces.Repositories;
 using Escola.Infra.Data.Context;
 using Escola.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +22,17 @@ builder.Services.AddDbContext<EscolaDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(Escola.Application.Mappings.MappingProfile));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Projeto Gestão Escolar - Documentação Web API",
+        Description = "Projeto fictício para gestão de escola com o objetivo de explorar as tecnlogias das quais eu possuo domínio.",
+        Contact = new OpenApiContact() { Name = "Levi Natanael", Email = "levi.natanael@gmail.com" },
+        License = new OpenApiLicense() { Name = "MIT License", Url = new Uri("https://opensource.org/licenses/MIT") }
+    });
+});
 
 builder.Services.AddScoped<IAlunoService, AlunoService>();
 builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
